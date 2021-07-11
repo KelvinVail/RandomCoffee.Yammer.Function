0bc23fb7d14eecfb0003c32009b65b8af191f279
Kelvin Vail <26182999+KelvinVail@users.noreply.github.com>
2021-07-11 13:43:43 +0100

    Add test project

---
+++ src/RandomCoffee.Yammer.Function.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RandomCoffee.Yammer.Function.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
